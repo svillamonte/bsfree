afb3b7426ca07b062d21c3db38ca4ceecb08c337
Sebastián Villamonte <walden2av@gmail.com>
2018-11-08 20:18:09 +1300

    Scaffolded app and did some experiments loading custom local data.

---
+++ src/BSFree/Services/Shout.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BSFree.Services
+{
+    public class Shout
+    {
+        public string ShoutId { get; set; }
+        public string Nick { get; set; }
+        public string Body { get; set; }
+        public string ImageUri { get; set; }
+    }
+}
